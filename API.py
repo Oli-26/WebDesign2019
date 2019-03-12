b02efe76359e15a71151543a434399927c8714fe
Oli-26 <Olivermholder@gmail.com>
2019-03-12 14:22:03 +0100

    db setup half working

--- API.py
+++ API.py
@@ -3,11 +3,20 @@ import json
 import flask
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from __init__ import app
+from __init__ import app, db
+from Core.airport import Airport
+
+
 jsondir = "airlines.json"
 
 
 
+airport1 = Airport(c = "AAA", n = "American Airlines Air")
+db.session.add(airport1)
+db.session.commit()
 
+
+airports = Airport.query.all()
+print(airports)
 
 app.run(port='5002', ssl_context='adhoc')  
--- __init__.py
+++ __init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 print(SQLALCHEMY_DATABASE_URI)
 #SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+from Core.airport import Airport
+db.init_app(app)
+
+db.create_all()
