5490d9ac906556b0cd1f5e970786d296d20f460c
NikDijkema <nikdijkema@hotmail.com>
2019-03-12 15:21:30 +0100

    conventions fix

--- Core/delays_minutes.py
+++ Core/delays_minutes.py
@@ -1,62 +1,62 @@
 from __init__ import db
 
 
-class DelaysMinutes(db.Model):
+class Delays_minutes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    late_aircraft = db.Column(db.Integer)
+    lateAircraft = db.Column(db.Integer)
     carrier = db.Column(db.Integer)
     security = db.Column(db.Integer)
     weather = db.Column(db.Integer)
-    national_aviation_system = db.Column(db.Integer)
+    nationalAviationSystem = db.Column(db.Integer)
     total = db.Column(db.Integer)
 
     def __init__(self, la, c, s, w, nas, t):
-        self.late_aircraft = la
+        self.lateAircraft = la
         self.carrier = c
         self.security = s
         self.weather = w
-        self.national_aviation_system = nas
+        self.nationalAviationSystem = nas
         self.total = t
 
     @staticmethod
-    def get_late_aircraft(self):
-        return self.late_aircraft
+    def getLateAircraft(self):
+        return self.lateAircraft
 
     @staticmethod
-    def get_carrier(self):
+    def getCarrier(self):
         return self.carrier
 
     @staticmethod
-    def get_security(self):
+    def getSecurity(self):
         return self.security
 
     @staticmethod
-    def get_weather(self):
+    def getWeather(self):
         return self.weather
 
     @staticmethod
-    def get_national_aviation_system(self):
-        return self.national_avaiation_system
+    def getNationalAviationSystem(self):
+        return self.nationalAvaiationSystem
 
     @staticmethod
-    def get_total(self):
+    def getTotal(self):
         return self.total
 
-    def set_late_aircraft(self, la):
-        self.late_aircraft = la
+    def setLateAircraft(self, la):
+        self.lateAircraft = la
 
-    def set_carrier(self, c):
+    def setCarrier(self, c):
         self.carrier = c
 
-    def set_security(self, s):
+    def setSecurity(self, s):
         self.security = s
 
-    def set_weather(self, w):
+    def setWeather(self, w):
         self.weather = w
 
-    def set_national_avaition_system(self, nas):
-        self.national_aviation_system = nas
+    def setNationalAvaitionSystem(self, nas):
+        self.nationalAviationSystem = nas
 
-    def set_total(self, new_total):
-        self.total = new_total
+    def setTotal(self, new_total):
+        self.total = newTotal
 
