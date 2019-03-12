70db00a0a5def3391c2dc786c47a9a3fb3baf798
Oli-26 <Olivermholder@gmail.com>
2019-03-12 13:53:32 +0100

    stucture cahnges

--- API.py
+++ API.py
@@ -3,9 +3,7 @@ import json
 import flask
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-
-
-app = Flask(__name__)
+from __init__ import app
 jsondir = "airlines.json"
 
 
--- __init__.py
+++ __init__.py
@@ -3,11 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-print("test1")
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 print(SQLALCHEMY_DATABASE_URI)
 #SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
-print("test2")
+
 db = SQLAlchemy(app)
