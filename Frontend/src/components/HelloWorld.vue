eaa6f84adda2d47df5b71a6ed5b2e4f44d845123
NikDijkema <nikdijkema@hotmail.com>
2019-03-24 19:35:24 +0100

    Routing demo added

--- Frontend/src/components/HelloWorld.vue
+++ Frontend/src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="hello">
+
+    <router-link to="/">Routing demonstration</router-link>
+
     <img src="../assets/logo.png" />
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
--- Frontend/src/components/Main.vue
+++ Frontend/src/components/Main.vue
@@ -2,5 +2,6 @@
 	<div class="hello">
 		<h1>This is our super fancy frontend.</h1>
 		<p>Components are next.</p>
+		<router-link to="/HelloWorld">Routing demonstration</router-link>
 	</div>
 </template>
