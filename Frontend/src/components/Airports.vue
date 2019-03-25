c6646337ba531e8b440836a734339d520d07e934
Oli-26 <Olivermholder@gmail.com>
2019-03-25 13:37:25 +0100

    forgot to add component

---
+++ Frontend/src/components/Airports.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="Airports">
+		<h1>Airports</h1>
+		<p>Cards containing all airports.</p>
+		<router-link to="/HelloWorld">Routing demonstration</router-link>
+	</div>
+</template>
