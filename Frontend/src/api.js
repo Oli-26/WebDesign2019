a01e4ee65900c54d17100ad1b27813c05947158a
Oli-26 <Olivermholder@gmail.com>
2019-03-26 03:27:23 +0100

    /airports front end now functional, links are currently dead though

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,10 +1,28 @@
 import axios from 'axios'
 
-export function testFunction () {
-        var resp = axios.get('/airports')
-            .then( response => {
-                return response
-            })
-        console.log(resp);
-        return resp;
+export function testFunction()  {
+        //var resp = axios.get('/airports/ATL?content-type=text/csv')
+        //    .then( response => {
+        //        return response.data
+       //     })
+            
+        //let list = {};
+        
+          try {
+            return axios.get('/airports')
+          } catch (error) {
+            console.error(error)
+          }
+        
+        //list = axios.get('/airports').then(function(result) {
+          
+          //  console.log(result.data)
+         //   list = result.data
+        //    console.log(result.data[0])
+            
+      // });
+        
+       // return getAirports;
+        
+        
 }
--- Frontend/src/components/Main.vue
+++ Frontend/src/components/Main.vue
@@ -1,19 +1,37 @@
 <template>
 	<div class="hello">
 		<h1>This is our super fancy frontend.</h1>
-		<p>Components are next. {{ response }}</p>
+		<p>Components are next. </p>
 		<router-link to="/HelloWorld">Routing demonstration</router-link>
-		<sui-card>
-	      <img src="../assets/logo.png" />
-	      <sui-card-content>
-	        <sui-card-header>Card demo</sui-card-header>
-	        <sui-card-meta>Joined in 2013</sui-card-meta>
-	        <sui-card-description>Kristy is an art director living in New York.</sui-card-description>
-	      </sui-card-content>
-	      <sui-card-content extra>
-	        <sui-icon name="user" />
-        22 Friends</sui-card-content>
-    </sui-card>
+       
+     
+       <div>
+    <div v-for="airport in airports">  
+    
+       
+
+     
+    
+        <a class="item" v-bind:href=airport.uri target="_blank">    
+            <sui-card-group :items-per-row="3">
+                  
+                <sui-card v-for="airport in airports">
+                    <img src="../assets/logo.png" />
+                    <sui-card-content>
+                        <sui-card-header> {{airport.name }} </sui-card-header>
+                        
+                      </sui-card-content>
+                      <sui-card-content extra>
+                        <sui-icon name="user" />
+                        Airport
+                        </sui-card-content>
+                    
+                </sui-card>
+            
+            </sui-card-group>
+        </a>    
+    </div>
+  </div>
 	</div>
 </template>
 
@@ -23,14 +41,26 @@
         
         data () {
             return {
-                response: "None"
+                airports: []
             }
         },        
         created() {
-            this.response = testFunction()
+            
+            testFunction()
+                .then(response => {
+                    console.log(response.data)
+                    for(var i = 0; i < response.data.length; i++){
+                        console.log(response.data[i].namw)
+                        if(response.data[i] != null){
+                            this.airports.push(response.data[i])
+                        }
+                    }
+                    //return response.data
+                    
+                })
                
         }
-        
+       
         
     }
     
