7dc1fc6119d01416552c5fcf4b3b92f7a1f79afd
ttc0124 <t.chen.4@student.rug.nl>
2019-03-26 15:56:54 +0100

    vue pages

---
+++ Frontend/src/components/Carriers.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="Carriers">
+		<h1>Carriers</h1>
+		<p>This is carriers page with carrierID {{ this.$route.params.carrierID }}</p>
+		<router-link to="/HelloWorld">Routing demonstration</router-link>
+	</div>
+</template>
