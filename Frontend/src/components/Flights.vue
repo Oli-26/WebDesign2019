ea539513601e3f539f3e1ffe9bf88a4399d968e4
NikDijkema <nikdijkema@hotmail.com>
2019-03-28 13:08:34 +0100

    UI updates, work to refresh statistics is month changes

--- Frontend/src/components/Flights.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="Flights">
-		<h1>Flights</h1>
-		<p>This is flights page with carrierID {{ this.$route.params.carrierID }}</p>
-		<router-link to="/carriers/:carrierID/statistics">Back to statistics with {{ this.$route.params.carrierID }}</router-link>
-	</div>
-</template>
